011bffbfa4830aecf5f122b8e200d6f55dd05b5f
sarveshhg-github <sarveshsavaliya1541@gmail.com>
2024-05-15 13:43:10 +0530

    added queries

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import express from 'express'
 import routes from './src/routes/index.js'
-
-
+import db from './src/config/db.js'
 const app = express()
+
 app.use(express.json())
+db.connect((err) => {
+    if (err) {
+        console.log(err.message)
+    }
+    else {
+        console.log("Connected to PalmsTownHall Database")
+    }
+})
 app.use('/', routes)
 
 app.listen(3000, () => console.log("express server initialized successfully"))
---
+++ src/constants/Queries.js
@@ -0,0 +1,9 @@
+export default {
+    LOGIN: "SELECT * FROM users WHERE (email,password)= (?,?)",
+    SIGNUP: "INSERT INTO users (name,username,email,phonenumber,usertype,password) VALUES (?,?,?,?,?,?)",
+    IF_EMAIL_EXIST: "SELECT * FROM users WHERE email =?",
+    SEND_OTP: "INSERT INTO OTP (email,OTP) VALUES(?,?)",
+    VERIFY_OTP: "SELECT * FROM OTP WHERE (email,OTP) = (? ,?)",
+    UPDATE_PASSWORD: "UPDATE users SET password=? WHERE email=?"
+
+}

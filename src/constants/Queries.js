7662fb5e3b9e64d3788c96256b4123e05e0c9ee7
sarveshhg-github <sarveshsavaliya1541@gmail.com>
2024-05-15 14:16:49 +0530

    added login route

--- src/constants/Queries.js
+++ src/constants/Queries.js
@@ -1,5 +1,5 @@
 export default {
-    LOGIN: "SELECT * FROM users WHERE (email,password)= (?,?)",
+    LOGIN: "SELECT * FROM users WHERE email=?",
     SIGNUP: "INSERT INTO users (name,username,email,phonenumber,usertype,password) VALUES (?,?,?,?,?,?)",
     IF_EMAIL_EXIST: "SELECT * FROM users WHERE email =?",
     SEND_OTP: "INSERT INTO OTP (email,OTP) VALUES(?,?)",
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import mainRoute from './user/mainRoute.js'
+import mainRoute from './mainRoute.js'
+import login from './user/login.js'
 
 const routes = express.Router()
 
 routes.use(
     '/', mainRoute
 )
+routes.use('/user/login', login)
 
 export default routes
